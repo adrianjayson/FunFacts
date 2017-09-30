f41b18fa22d9d5283eff4a66ee7362527fbb34a7
Adrian <adrian@MBP.local>
2017-09-30 11:26:26 +0800

    Initial commit

---
+++ app/src/main/java/adrianjayson/com/funfacts/ColorWheel.java
@@ -0,0 +1,36 @@
+package adrianjayson.com.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by adrian on 16/09/2017.
+ */
+
+public class ColorWheel {
+    private final String[] COLORS = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    public int getColor() {
+        // Randomly select a fact
+        Random randomGenerator = new Random();
+        int randColor = randomGenerator.nextInt(COLORS.length);
+        int color = Color.parseColor(COLORS[randColor]);
+
+        return color;
+    }
+}
